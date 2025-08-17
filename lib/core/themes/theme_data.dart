8370d0545ed279c1df710b7483289ae9dd59bf6a
Ali kamar <aliekamar123@gmail.com>
2025-08-17 21:32:00 +0300

    add AppTheme class for defining light theme with Urbanist font and primary colors

---
+++ lib/core/themes/theme_data.dart
@@ -0,0 +1,10 @@
+import 'package:finance_app/core/themes/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    fontFamily: 'Urbanist',
+    primaryColor: AppColors.primaryColor,
+    scaffoldBackgroundColor: AppColors.whiteColor,
+  );
+}
