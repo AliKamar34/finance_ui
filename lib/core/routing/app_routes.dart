4c33aa645d89c0eb4d6a37966be42788970a180e
Ali kamar <aliekamar123@gmail.com>
2025-08-19 02:34:20 +0300

    Add routing configuration for authentication and onboarding views

---
+++ lib/core/routing/app_routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  static const String onBoarding = '/onBoarding';
+  static const String login = '/login';
+  static const String singUp = '/singUp';
+  static const String forgetPassword = '/forgetPassword';
+  static const String passwordChanged = '/passwordChanged';
+  static const String createNewPassword = '/createNewPassword';
+}
---
+++ lib/core/routing/router_generation_config.dart
@@ -0,0 +1,46 @@
+import 'package:finance_app/core/routing/app_routes.dart';
+import 'package:finance_app/features/auth/presentation/views/create_new_password_view.dart';
+import 'package:finance_app/features/auth/presentation/views/forget_password_view.dart';
+import 'package:finance_app/features/auth/presentation/views/log_in_view.dart';
+import 'package:finance_app/features/auth/presentation/views/password_changed_view.dart';
+import 'package:finance_app/features/auth/presentation/views/sign_up_view.dart';
+import 'package:finance_app/features/onBoarding/views/on_boarding_view.dart';
+import 'package:go_router/go_router.dart';
+
+class RouterGenerationConfig {
+  static GoRouter goRoute = GoRouter(
+    initialLocation: AppRoutes.onBoarding,
+    routes: [
+      GoRoute(
+        path: AppRoutes.onBoarding,
+        name: AppRoutes.onBoarding,
+        builder: (context, state) => const OnBoardingView(),
+      ),
+      GoRoute(
+        path: AppRoutes.login,
+        name: AppRoutes.login,
+        builder: (context, state) => const LogInView(),
+      ),
+      GoRoute(
+        path: AppRoutes.singUp,
+        name: AppRoutes.singUp,
+        builder: (context, state) => const SignUpView(),
+      ),
+      GoRoute(
+        path: AppRoutes.forgetPassword,
+        name: AppRoutes.forgetPassword,
+        builder: (context, state) => const ForgetPasswordView(),
+      ),
+      GoRoute(
+        path: AppRoutes.createNewPassword,
+        name: AppRoutes.createNewPassword,
+        builder: (context, state) => const CreateNewPasswordView(),
+      ),
+      GoRoute(
+        path: AppRoutes.passwordChanged,
+        name: AppRoutes.passwordChanged,
+        builder: (context, state) => const PasswordChangedView(),
+      ),
+    ],
+  );
+}
