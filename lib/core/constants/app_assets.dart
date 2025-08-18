0ca9de01e906930a24ef93beb2e148521c1f81cb
Ali kamar <aliekamar123@gmail.com>
2025-08-19 02:35:14 +0300

    Add SVG assets for Facebook, Google, Apple, and Success logos

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,3 +1,7 @@
 class AppAssets {
   static const String onBoarding1 = 'assets/images/onBoarding1.png';
+  static const String faceBookLogo = 'assets/images/faceBookLogo.svg';
+  static const String googleLogo = 'assets/images/google.svg';
+  static const String appleLogo = 'assets/images/apple.svg';
+  static const String success = 'assets/images/success.svg';
 }
