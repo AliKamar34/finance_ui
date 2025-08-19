86fd015e8a06f5bd609cd933d7148eada1a84b37
Ali kamar <aliekamar123@gmail.com>
2025-08-19 20:28:46 +0300

    Add new SVG icons and update asset constants

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -4,4 +4,16 @@ class AppAssets {
   static const String googleLogo = 'assets/images/google.svg';
   static const String appleLogo = 'assets/images/apple.svg';
   static const String success = 'assets/images/success.svg';
+  static const String chart = 'assets/icons/Chart.svg';
+  static const String download = 'assets/icons/Downloads.svg';
+  static const String editSquare = 'assets/icons/EditSquare.svg';
+  static const String groupUser = 'assets/icons/GroupUser.svg';
+  static const String home = 'assets/icons/Home.svg';
+  static const String more = 'assets/icons/more.svg';
+  static const String notification = 'assets/icons/Notification.svg';
+  static const String plus = 'assets/icons/Plus.svg';
+  static const String profile = 'assets/icons/Profile.svg';
+  static const String send = 'assets/icons/Send.svg';
+  static const String upload = 'assets/icons/Upload.svg';
+  static const String wallet = 'assets/icons/Wallet.svg';
 }
